da23ec14119c309da45275cde9326ada0094a714
Karen <13109563292@163.com>
2020-12-02 14:15:03 +0800

    123

---
+++ src/main/java/org/westos/demo01/Test1.java
@@ -0,0 +1,27 @@
+package org.westos.demo01;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+/**
+ * @Author: JiaLun
+ * @Description:
+ * @Date: Created in 2020/12/1 18:52
+ * @Modified By:
+ */
+public abstract class Test1 {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        set.add(3);
+        System.out.println(set);
+    }
+
+
+}
+
+
---
+++ src/main/java/org/westos/demo01/Test2.java
@@ -0,0 +1,13 @@
+package org.westos.demo01;
+
+/**
+ * @Author: JiaLun
+ * @Description:
+ * @Date: Created in 2020/12/1 18:54
+ * @Modified By:
+ */
+public interface Test2 {
+    public static final Integer a = 0;
+
+
+}
